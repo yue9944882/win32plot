3cfd5750d6818e1fb1d74fab183a584153e97aae
Kimmin <291271447@qq.com>
2015-10-28 15:57:56 +0800

    Operator Camparision

--- parse_polynomial/syn_ana.cpp
+++ parse_polynomial/syn_ana.cpp
@@ -48,7 +48,46 @@ int main(){
 
 int parse_poly(std::string expr){
 	
+	char*stack_buf=new char[1024];
+	
+	int stack_pos=0;
+	int input_pos=0;
+	
+	//Pushing '#' into stack
+	stack_buf[stack_pos]='#';
+	stack_pos++;
+	
+	while(stack_buf[stack_pos]!='#'){
+		
+		
+		
+		
+		
+		
+	}	
+	
+	delete []stack_buf;
+
+}
+ 
+ // VT-camparision GRAPH
+ // -1 for < , +1 for > , 0 for equal
+ // 
+ //		\d	 +  -   * 	 / 	 .  ( 	 )  # 	 ^
+ //	\d	-1  +1	+1	+1	+1	-1	-1 	+1	+1	+1 
+ // + 	-1 	+1	+1	-1	-1 		-1 	+1	+1 	-1 
+ //	- 	-1	+1	+1	-1	-1 		-1 	+1	+1 	-1 
+ // * 	-1	+1	+1	+1	+1 		-1 	+1	+1 	-1 
+ //	/ 	-1	+1	+1	+1	+1 		-1 	+1	+1 	-1 
+ //	. 	-1							
+ //	(	-1	-1	-1	-1	-1 		-1 	0 	 	-1 
+ //	)	+1	+1	+1	+1	+1 		 	+1 	+1 	+1 
+ //	#	-1	-1	-1	-1 	-1 		-1 	 	0 	-1 
+ // ^	-1	+1 	+1	+1 	+1 		-1	+1 	+1 	-1 
+ 
+ 
+int VT_cmp(char c1,char c2){
+	
 	
 }
-
 
